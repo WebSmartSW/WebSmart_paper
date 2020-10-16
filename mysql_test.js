fd377ecbb4f8a0a56f4600ad5e7027e365a2943c
wjswls735 <wjswls735@naver.com>
2020-10-16 11:57:07 +0900

    update mysql_test.js

--- mysql_test.js
+++ mysql_test.js
@@ -13,8 +13,49 @@ conn.query(sql, function(err, rows, fields){
         console.log(err);
     }
     else{
-        console.log('rows', rows);
-        console.log('fields', fields);
+        for(var i=0; i<rows.length; i++){
+            console.log(rows[i].title);
+        }
+        // console.log('rows', rows);
+        // console.log('fields', fields);
+    }
+});
+// var sql = 'INSERT INTO topic (title, description, author) VALUES("?", "?", "?")';
+// var params= ['Supervisor', 'Watcher', 'graphittie'];
+
+// conn.query(sql, params, function(err, rows, fields){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{        
+//         console.log(rows.insertId);
+//         // console.log('fields', fields);
+//     }
+// });
+// var sql = 'UPDATE topic SET title=?, author=? WHERE id=?';
+
+// var params= ['NPM', 'leezche', '1'];
+
+// conn.query(sql, params, function(err, rows, fields){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{        
+//         console.log(rows);
+//         // console.log('fields', fields);
+//     }
+// });
+var sql = 'DELETE FROM topic WHERE id=?';
+
+var params= [6];
+
+conn.query(sql, params, function(err, rows, fields){
+    if(err){
+        console.log(err);
+    }
+    else{        
+        console.log(rows);
+        // console.log('fields', fields);
     }
 });
 conn.end();
