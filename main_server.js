414f901c7b2cd1433cee0b60220482683c53d1e4
wjswls735 <wjswls735@naver.com>
2020-11-12 13:52:30 +0900

    edit

--- main_server.js
+++ main_server.js
@@ -1,17 +1,18 @@
 var http = require('http');
 var fs = require('fs');
+var express = require("express");
+var gd_app = express();
+
+gd_app.post("/create", function(req, res){
+  console.log('------------------');
+  console.dir(req);
+  res.redirect('/');
+});
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/participation_application.html';
-    }
-    if(request.url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
+    url = '/participation_application.html';
     response.writeHead(200);
     response.end(fs.readFileSync(__dirname + url));
- 
 });
+
 app.listen(3000);
