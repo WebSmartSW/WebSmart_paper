87a12d55256c4c2b016ca8a24160efa5c81621a6
leehaeina <leehaein0164@gmail.com>
2020-12-03 19:04:50 +0900

    apply create

--- js/participation.js
+++ js/participation.js
@@ -5,20 +5,21 @@ function applyForm(){
    let description = document.getElementById("description").value;
    let title = document.getElementById("title").value;
    let leaderName = document.getElementById("leaderName").value;
-   let ledaerId = document.getElementById("leaderId").value;
+   let leaderId = document.getElementById("leaderId").value;
    let teamName = document.getElementById("teamName").value;
    let phone = document.getElementById("phone").value;
    let email = document.getElementById("email").value;
-   console.log(description,title,leaderName,ledaerId,teamName,phone,email)
-//    $.ajax({
-//     type: "create",
-//     url: 'http://localhost:7000/admin/team',
-//      datatype: 'json',
-//      contentType: "application/json; charset=utf-8",
-//     data:JSON.stringify({locationId:locationId,destDescription:description||null,destId:destId}),
-//    success: function(data){
+   //console.log(description,title,leaderName,ledaerId,teamName,phone,email)
+   $.ajax({
+    type: "post",
+    url: 'http://localhost:7000/admin/apply',
+     datatype: 'json',
+     contentType: "application/json; charset=utf-8",
+    data:JSON.stringify({description:description,title:title||null,leaderName:leaderName,leaderId:Number(leaderId),
+        teamName:teamName,phone:phone,email:email}),
+   success: function(data){
 
-//      console.log(data)
-//      }
-//     }) 
+     console.log(data)
+     }
+    }) 
 }
